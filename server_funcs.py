886741f142e9e6ff05d202344a3b99ee694d4f85
anna-sara-maria <ak9522@gmail.com>
2023-02-09 10:41:17 +0100

    Update server_funcs.py

--- server_funcs.py
+++ server_funcs.py
@@ -1,15 +1,19 @@
 import smtplib
+from email.message import EmailMessage
 import ssl
 
 def build(body):
+	pass
 	#takes output from parse_post_json, clones the repo from git
 	#compiles code - lints python (flake8)
 	#return 0 on success, 1 on fail
 
 def test():
+	pass
 	#runs tests
 
 def save_results():
+	pass
 	#creates folder for the commit results - perhaps a separate function called from here
 	#creates file with results in folder
 		#dependency versions and commit id included
@@ -24,11 +28,13 @@ def save_results():
                   #error message?        #post to api.git (commit status)
 
 def restore():
+	pass
 	#deletes the cloned repo and compiled code in preparation for next webhook
 	
 	
 	
 def notify():
+	pass
 	# when code is received
 	#	send emails to everyone to tell them code was recieved
 	# then tests are done
@@ -37,24 +43,21 @@ def notify():
 
 def send_email(receiver_email, message):
 	sender_email = "continuousintegration2023@gmail.com"
-	receiver_email = "continuousintegration2023@gmail.com"
-	# password = input(str(“please enter your password : ”))
-
-	message = "Hey, this was sent using python :D" 
-
 	simple_email_context = ssl.create_default_context()
-
 	smtp_port = 587			# Standard secure SMTP port
 	smtp_server = "smtp.gmail.com"  # Google SMTP Server
 
 	try:
 		server = smtplib.SMTP(smtp_server, smtp_port)
 		server.starttls(context=simple_email_context)
-		server.login(sender_email, "bengbeng%ED")
-		print("Login success")
-		print("Sending email to {receiver_email}")
+		server.login(sender_email, "bmvk xgju anqh kloq")#password) #"bengbeng%ED")
 		server.sendmail(sender_email, receiver_email, message) # Sends email
-		print("Email has been sent to {receiver_email}")
+		print("Email has been sent to", receiver_email)
 
 	except Exception as e:
 		print(e)
+
+print("\n \n \n")
+receiver_email = "ak9522@gmail.com"
+message = "Python test  :D" 
+send_email(receiver_email, message)
