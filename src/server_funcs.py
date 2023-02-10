f5e6e0eaf813fc4ee006e826ff238065a87824f8
anna-sara-maria <60351058+anna-sara-maria@users.noreply.github.com>
2023-02-10 17:57:59 +0100

    Feat Notify

--- src/server_funcs.py
+++ src/server_funcs.py
@@ -146,24 +146,13 @@ def restore():
     # deletes the cloned repo and compiled code in preparation for next webhook
 
 
-def notify():
+def send_email(message):
     """
-    TBD
+    Sends an email with information about the commit and the test results
     """
-    print()
-    # when code is received
-    #	send emails to everyone to tell them code was recieved
-    # then tests are done
-    # test results are sent to ... everyone? or just the person who sent the code?
-
-
-def send_email(receiver_email, message):
-    """
-    Sends an email with the test results
-    """
+    receiver_email = "gustawsi@kth.se,adriankv@kth.se"
     sender_email = "continuousintegration2023@gmail.com"
-    # the password will be integrated into the code but not here on github :)
-    password = input(str("please enter your password : "))
+    #password = input(str("please enter your password : "))
     simple_email_context = ssl.create_default_context()
     smtp_port = 587			# Standard secure SMTP port
     smtp_server = "smtp.gmail.com"  # Google SMTP Server
@@ -171,7 +160,7 @@ def send_email(receiver_email, message):
     try:
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls(context=simple_email_context)
-        server.login(sender_email, password)
+        server.login(sender_email, "bmvk xgju anqh kloq")
         server.sendmail(sender_email, receiver_email, message)  # Sends email
         print("Email has been sent to", receiver_email)
 
